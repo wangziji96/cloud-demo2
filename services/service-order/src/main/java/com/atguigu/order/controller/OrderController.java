25181462ec4bca9bf3cc4c988105ca43ab3cb146
wzj <wzj@git.com>
2026-05-04 23:25:12 +0800

    网关引入负载均衡依赖，配置nacos地址和路由规则

--- services/service-order/src/main/java/com/atguigu/order/controller/OrderController.java
+++ services/service-order/src/main/java/com/atguigu/order/controller/OrderController.java
@@ -7,10 +7,12 @@ import com.atguigu.order.properties.OrderProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //@RefreshScope
+@RequestMapping("/api/order")
 @Slf4j
 @RestController
 public class OrderController {
--- services/service-order/src/main/java/com/atguigu/order/frign/ProductFeignClient.java
+++ services/service-order/src/main/java/com/atguigu/order/frign/ProductFeignClient.java
@@ -6,11 +6,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(value = "service-product",fallback = ProductFeignClientFallback.class)
 public interface ProductFeignClient {
 
-    @GetMapping("/product/{id}")
+    @GetMapping("/api/product/product/{id}")
     Product getProductById(@PathVariable("id")Long id);
 
 }
